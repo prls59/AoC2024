8da8f8ee4fb9c172799ea347a531552f7974c872
prls59 <119790965+prls59@users.noreply.github.com>
2024-12-08 17:20:10 +0000

    Days 7 & 8

---
+++ 07 Bridge Repair/part2.py
@@ -0,0 +1,33 @@
+import os, math
+
+DATAFILE = "input.txt"
+
+def evaluate(target, left, right):
+    x = right[0]
+    if len(right) == 1:
+        if left * x == target or left + x == target:
+            return target
+        else:
+            return left * 10**(int(math.log10(x)) + 1) + x
+    else:
+        rest = right[1:]
+        if left * x <= target:
+            result = evaluate(target, left * x, rest)
+            if result == target:
+                return target
+        result = evaluate(target, left + x, rest)
+        if result == target:
+            return target
+        return evaluate(target, left * 10**(int(math.log10(x)) + 1) + x, rest)
+
+calibration_result = 0
+
+with open(os.path.dirname(os.path.abspath(__file__)) + "/" + DATAFILE) as input:
+    for line in input:
+        equation = line[0:-1].split(': ')
+        target = int(equation[0])
+        values = [int(x) for x in equation[1].split()]
+        if evaluate(target, values[0], values[1:]) == target:
+            calibration_result += target
+
+print('Result = ', calibration_result)
---
+++ 08 Resonant Collinearity/part2.py
@@ -0,0 +1,53 @@
+import os, re
+from collections import defaultdict
+
+datafile = "input.txt"
+
+unique_locations = set()
+antennae = defaultdict(list)
+city_limits = []
+
+def find_antinodes(ant1, ant2, unique_locations, city_limits):
+    dx = ant1[0] - ant2[0]
+    dy = ant1[1] - ant2[1]
+    factor = 0
+    out_of_bounds_1 = False
+    out_of_bounds_2 = False
+    
+    while not (out_of_bounds_1 and out_of_bounds_2):
+        if not out_of_bounds_1:
+            new_x = ant1[0] + dx * factor
+            new_y = ant1[1] + dy * factor
+            if new_x >=0 and new_x < city_limits[0] and new_y >=0 and new_y < city_limits[1]:
+                unique_locations.add((new_x, new_y))
+            else:
+                out_of_bounds_1 = True
+        if not out_of_bounds_2:
+            new_x = ant2[0] - dx * factor
+            new_y = ant2[1] - dy * factor
+            if new_x >=0 and new_x < city_limits[0] and new_y >=0 and new_y < city_limits[1]:
+                unique_locations.add((new_x, new_y))
+            else:
+                out_of_bounds_2 = True
+        factor += 1
+
+with open(os.path.dirname(os.path.abspath(__file__)) + "/" + datafile) as input:
+    y = 0
+    width = 0
+    for line in input:
+        width = len(line) - 1
+        antenna_matches = re.finditer('[a-zA-Z0-9]', line[:-1])
+        for antenna_match in antenna_matches:
+            antennae[antenna_match.group(0)].append([antenna_match.start(),y])
+        y += 1
+    city_limits = [width, y]
+
+for antenna_freq in iter(antennae):
+    linked_antennae = antennae.get(antenna_freq)
+    linked_count = len(linked_antennae)
+    if linked_count > 1:
+        for a in range(linked_count - 1):
+            for b in range(a + 1, linked_count):
+                  find_antinodes(linked_antennae[a], linked_antennae[b], unique_locations, city_limits)
+
+print('Result = ', len(unique_locations))
